36e980429139c2ec99ad6597eff92c6855bed56e
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate `Feelslike` into Settings
    Change-Id: If03bf0f9b60ff818e8ceb2b5d4c9f4b306739261

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -448,4 +448,9 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    /**
+     * Activity for The Feelslike
+     */
+    public static class SerasaSettingsActivity extends SettingsActivity {}
 }
