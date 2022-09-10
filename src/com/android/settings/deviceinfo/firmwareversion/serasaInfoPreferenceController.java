a8b34176e345e5c2809512d90895fce1e9c72a7a
str143 <alexfinhart@gmail.com>
2022-09-10 16:21:36 +0800

    [SQUASH]: Settings Homepage Redesign
    Co-authored-by: beingmishra <rahul731999@gmail.com>
    Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Change-Id: Ib544c0f75857452d7729961107fc90d04b50491e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Settings: Avoid NPE when adding/removing highlight background
    
    https://bin.kv2.dev/~636d47fde599576ea02b0ef3
    
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    
    settings: Match the searchbar color with the cards
    
    Change-Id: Ic0d4fec2d8dc4ebe4e1ea894ee33409c1c77b538
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Settings: add OOS 13 like banner to about phone section
    
    * based from dotOS about phone banner modified to card design
    
    original commit message:
    Based on previous implementation from DotOS 5.x with a new design
    
    Change-Id: I5d28862b322b65521506be0ce6291473061a0d96
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Co-authored-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    
    Settings: Align user avatar with homepage title
    
    [ghostrider-reborn: adapt to s-v2]
    
    Change-Id: I2693a1f19a46bc6d0df8596e12bea8ca19914f13
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    Add deep sleep preference controller [2/2]
    
    Change-Id: If270186b7788d95bfb4f555c0bc6f7eb3367e95f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Make kernel version clickable
    
    - so that it expands and shows full kernel version
    
    Change-Id: Ic08ec7a1db62a521e434cac5188aad2d35a4825d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [SQUASH]: Import settings changes and customizations
    
    Redesign Settings firmware version section
    
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    settings: Redesign settings and improve theming support
    
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    settings: Revamp Ayan's card UI style
    
    * based on notayan99's figma specs
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    settings: Introduce Settings Omni Headers/Banners [1/2]
    
    * Inspired by OmniRom QS headers
    * allow user to choose from prefixed collection of images
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    settings: Custom settings header text [1/2]
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    settings: Random settings header image [1/2]
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    
    settings: Introduce contextual dashboard messages [1/2]
    
    Co-authored-by: El Dainosor <eldainosor@gmail.com>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: beingmishra <rahul731999@gmail.com>
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
    Co-authored-by: notayan99 <ayan98542@gmail.com>
    Change-Id: I984f9ee42ab09b91200a9d5979b62842eb7923d5
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/serasaInfoPreferenceController.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2020 Wave-OS
+ * Copyright (C) 2023 the RisingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.widget.TextView;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.widget.LayoutPreference;
+
+public class serasa extends AbstractPreferenceController {
+
+    private static final String KEY_SERASA_INFO = "serasa_info";
+    private static final String KEY_SERASA_DEVICE = "serasa_device";
+    private static final String KEY_SERASA_VERSION = "serasa_version";
+    private static final String KEY_BUILD_STATUS = "rom_build_status";
+    private static final String KEY_BUILD_VERSION = "serasa_build_version";
+    
+    private static final String PROP_SERASA_VERSION = "ro.serasa.version";
+    private static final String PROP_SERASA_RELEASETYPE = "ro.serasa.buildtype";
+    private static final String PROP_SERASA_MAINTAINER = "ro.serasa.maintainer";
+    private static final String PROP_SERASA_DEVICE = "ro.serasa.device";
+    private static final String PROP_SERASA_BUILD_VERSION = "ro.serasa.build.version";
+
+
+    public serasaInfoPreferenceController(Context context) {
+        super(context);
+    }
+
+    private String getDeviceName() {
+        String device = SystemProperties.get(PROP_SERASA_DEVICE, "");
+        if (device.equals("")) {
+            device = Build.MANUFACTURER + " " + Build.MODEL;
+        }
+        return device;
+    }
+
+    private String getSerasaBuildVersion() {
+        final String buildVer = SystemProperties.get(PROP_SERASA_BUILD_VERSION,
+                this.mContext.getString(R.string.device_info_default));;
+
+        return buildVer;
+    }
+    
+    private String getSerasaVersion() {
+        final String version = SystemProperties.get(PROP_SERASA_VERSION,
+                this.mContext.getString(R.string.device_info_default));
+
+        return version;
+    }
+
+    private String getSerasaReleaseType() {
+        final String releaseType = SystemProperties.get(PROP_SERASA_RELEASETYPE,
+                this.mContext.getString(R.string.device_info_default));
+	
+        return releaseType.substring(0, 1).toUpperCase() +
+                 releaseType.substring(1).toLowerCase();
+    }
+    
+    private String getSerasabuildStatus() {
+	final String buildType = SystemProperties.get(PROP_SERASA_RELEASETYPE,
+                this.mContext.getString(R.string.device_info_default));
+        final String isOfficial = this.mContext.getString(R.string.build_is_official_title);
+	final String isCommunity = this.mContext.getString(R.string.build_is_community_title);
+	
+	if (buildType.toLowerCase().equals("official")) {
+		return isOfficial;
+	} else {
+		return isCommunity;
+	}
+    }
+
+    private String getSerasaMaintainer() {
+	final String SerasaMaintainer = SystemProperties.get(PROP_SERASA_MAINTAINER,
+                this.mContext.getString(R.string.device_info_default));
+	final String buildType = SystemProperties.get(PROP_SERASA_RELEASETYPE,
+                this.mContext.getString(R.string.device_info_default));
+        final String isOffFine = this.mContext.getString(R.string.build_is_official_summary, SerasaMaintainer);
+	final String isOffMiss = this.mContext.getString(R.string.build_is_official_summary_oopsie);
+	final String isCommMiss = this.mContext.getString(R.string.build_is_community_summary_oopsie);
+	final String isCommFine = this.mContext.getString(R.string.build_is_community_summary, SerasaMaintainer);
+	
+	if (buildType.toLowerCase().equals("official") && !SerasaMaintainer.equalsIgnoreCase("Unknown")) {
+	    return isOffFine;
+	} else if (buildType.toLowerCase().equals("official") && SerasaMaintainer.equalsIgnoreCase("Unknown")) {
+	     return isOffMiss;
+	} else if (buildType.equalsIgnoreCase("Community") && SerasaMaintainer.equalsIgnoreCase("Unknown")) {
+	     return isCommMiss;
+	} else {
+	    return isCommFine;
+	}
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        final Preference arcVerPref = screen.findPreference(KEY_SERASA_VERSION);
+        final Preference arcDevPref = screen.findPreference(KEY_SERASA_DEVICE);
+        final Preference buildStatusPref = screen.findPreference(KEY_BUILD_STATUS);
+        final Preference buildVerPref = screen.findPreference(KEY_BUILD_VERSION);
+        final String SerasaVersion = getSerasaVersion();
+        final String SerasaDevice = getDeviceName();
+        final String SerasaReleaseType = getSerasaReleaseType();
+        final String SerasaMaintainer = getSerasaMaintainer();
+	final String buildStatus = getSerasabuildStatus();
+	final String buildVer = getSerasaBuildVersion();
+	final String isOfficial = SystemProperties.get(PROP_SERASA_RELEASETYPE,
+                this.mContext.getString(R.string.device_info_default));
+	buildStatusPref.setTitle(buildStatus);
+	buildStatusPref.setSummary(SerasaMaintainer);
+	buildVerPref.setSummary(buildVer);
+        arcVerPref.setSummary(SerasaVersion);
+        arcDevPref.setSummary(SerasaDevice);
+	if (isOfficial.toLowerCase().contains("official")) {
+		 buildStatusPref.setIcon(R.drawable.verified);
+	} else {
+		buildStatusPref.setIcon(R.drawable.unverified);
+	}
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_SERASA_INFO;
+    }
+}
