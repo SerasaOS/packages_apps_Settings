4af882ac47b71a68a638ddee330382763aace9ac
strongreasons <abenkenari@gmail.com>
2023-09-26 16:53:59 +0000

    Settings: Redone package Installer
    Change-Id: I6720d4fa7347d902cd04fecb1f43db69cfcec152

--- tests/robotests/src/com/android/settings/applications/ApplicationFeatureProviderImplTest.java
+++ tests/robotests/src/com/android/settings/applications/ApplicationFeatureProviderImplTest.java
@@ -353,7 +353,7 @@ public final class ApplicationFeatureProviderImplTest {
 
         final Set<String> allowlist = mProvider.getKeepEnabledPackages();
 
-        assertThat(allowlist).contains("com.android.packageinstaller");
+        assertThat(allowlist).contains("com.serasa.packageinstaller");
     }
 
     private void setUpUsersAndInstalledApps() {
