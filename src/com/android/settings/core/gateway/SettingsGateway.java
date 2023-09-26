RealN00B/android_packages_apps_Settings

Allow to quickly open Running Services from meminfo view
@neobuddy89: Updated for A12.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -40,6 +40,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
@@ -350,7 +351,8 @@ public class SettingsGateway {
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
             TurnScreenOnDetails.class.getName(),
-            SuperiorLab.class.getName()
+            SuperiorLab.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -395,5 +397,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.SuperiorLabActivity.class.getName(),
             Settings.PreviouslyConnectedDeviceDashboardActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }

SerasaOS/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -40,6 +40,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
@@ -353,7 +354,8 @@ public class SettingsGateway {
             TurnScreenOnDetails.class.getName(),
             SerasaSettings.class.getName(),
             WifiTetherClientManager.class.getName(),
-            SmartPixels.class.getName()
+            SmartPixels.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -398,5 +400,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.SerasaSettingsActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }